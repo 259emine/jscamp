de5697b32193df3dcc3d084c945042749c7f427d
259emine <72345704+259emine@users.noreply.github.com>
2021-10-20 13:32:16 +0300

    Javascript Kampı

---
+++ src/app.js
@@ -0,0 +1,28 @@
+console.log("Merhaba Javascrıpt")
+
+let dolarBugun = 9.30
+
+let dolarDun = 9.20
+{
+    let dolarDun = 9.10
+}
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+//euroDun = 11//bu kısım hata yazılmaz 
+
+console.log(euroDun)
+
+let konutKredileri = ["Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi"]
+
+console.log("<ul>")
+for(let i = 0;i<konutKredileri.length;i++){
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
+
+console.log(konutKredileri)
+{
+    
+}
